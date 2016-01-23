9b044496b1214141f2206ba5071ff9d0c965c954
Quentin de Quelen <gitlab@dequelen.eu>
2016-01-23 14:35:58 +0100

    create the scarfolding with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name catstagramApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the catstagramApp
+ */
+angular.module('catstagramApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
